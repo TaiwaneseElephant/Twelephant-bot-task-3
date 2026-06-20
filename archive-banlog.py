07f271df2f735bcc865e868090c227d218c828ab
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-06-20 23:50:37 +0800

    Add files via upload

---
+++ archive-banlog.py
@@ -0,0 +1,48 @@
+import requests, json, os, re, datetime
+apiurl = "https://test.wikipedia.org/w/api.php"
+headers = {
+    "user-agent": "Twelephant-bot"
+}
+config = json.loads(list(requests.get(apiurl, headers=headers, params={"action":"query", "prop":"revisions", "rvprop":"content", \
+          "titles":"User:Twelephant-bot/task/3/config.json", "format":"json"}).json()["query"]["pages"].values())[0]["revisions"][0]["*"])
+pageid = config["pageid"]
+banlogtemplate = config["banlogtemplate"]
+banlogarchivepageheader = config["banlogarchivepageheader"]
+banlogarchivepagetitleformat = config["banlogarchivepagetitleformat"]
+banlogpage = requests.get(apiurl, headers=headers, params={"action":"query", "prop":"revisions", "rvprop":"content", "pageids":pageid, "format":"json"}).json()["query"]["pages"][pageid]
+banlogcontent = banlogpage["revisions"][0]["*"]
+banlogtitle = banlogpage["title"]
+banlogs = []
+now = datetime.datetime.now(datetime.timezone.utc)
+for ban in re.finditer(r"\{\{\s*%s\s*\|[^}]+?\}\}\s*" % banlogtemplate, banlogcontent):
+  ban = ban.group()
+  ban_has_end = re.search(r"\|\s*end\s*=\s*(\d+)", ban)
+  if ban_has_end:
+    date = datetime.datetime.strptime((ban_has_end.groups()[0]), "%Y%m%d%H%M").replace(tzinfo=datetime.timezone.utc)
+    if date < now:
+      banlogs.append([ban, date.year])
+      banlogcontent = banlogcontent.replace(ban, "")
+banlogarchivepages = {}
+for ban, year in banlogs:
+  if year in banlogarchivepages.keys():
+    banlogarchivepages[year] += ban
+  else:
+    banlogarchivepage = list(requests.get(apiurl, headers=headers, params={"action":"query", \
+    "prop":"revisions", "rvprop":"content", "titles":(banlogarchivepagetitleformat % (banlogtitle, year)), "format":"json"}).json()["query"]["pages"].values())[0]
+    if"missing" in banlogarchivepage.keys():
+      banlogarchivepages[year] = banlogarchivepageheader + ban
+      print(banlogarchivepagetitleformat % (banlogtitle, year))
+    else:
+      banlogarchivepages[year] = banlogarchivepage["revisions"][0]["*"] + ban
+print(banlogarchivepages)
+if len(banlogarchivepages.keys()) > 0:
+  session = requests.Session()
+  logintoken = session.get(apiurl, headers=headers, params={"action":"query", "meta":"tokens", "type":"login", "format":"json"}).json()["query"]["tokens"]["logintoken"]
+  session.post(apiurl, headers=headers, params={"action":"login"}, data={"lgname":"Twelephant-bot", "lgpassword":os.envar("BOTPWD"), "lgtoken":logintoken})
+  csrftoken = session.get(apiurl, headers=headers, params={"action":"query", "meta":"tokens", "type":"csrf", "format":"json"}).json()["query"]["tokens"]["csrftoken"]
+  for year, content in banlogarchivepages.items():
+    csrftoken = session.get(apiurl, headers=headers, params={"action":"query", "meta":"tokens", "type":"csrf", "format":"json"}).json()["query"]["tokens"]["csrftoken"]
+    session.post(apiurl, headers=headers, params={"action":"edit"}, data={"title":(banlogarchivepagetitleformat % (banlogtitle, year)), \
+                                                                          "text":content, "summary":"自動存檔已過期的禁制", "minor":True, "bot":True, "token":csrftoken})
+  session.post(apiurl, headers=headers, params={"action":"edit"}, data={"pageid":pageid, "text":banlogcontent, "summary":"自動存檔已過期的禁制", \
+                                                                        "minor":True, "bot":True, "token":csrftoken})
